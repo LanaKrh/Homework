33dc31fba601d6eade6368be4f84d7cdb0dd6d67
LanaKrh <124197026+LanaKrh@users.noreply.github.com>
2023-02-13 15:49:13 +0100

    Add files via upload

---
+++ project/Project.java
@@ -0,0 +1,10 @@
+package project;
+
+public class Project {
+    public static void main(String [] args) {
+        String text = "Это моя первая программа.";
+        String text1 = "Надеюсь она получилась хотя бы на 10%";
+        System.out.println(text + text1);
+
+    }
+}
